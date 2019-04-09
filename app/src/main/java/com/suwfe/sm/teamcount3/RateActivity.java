0e6f9cc5082b3e4f66ccee97203a4df40f5cc3ce
liusimeng123 <160689245@qq.com>
2019-04-09 20:25:29 +0800

    try3

--- app/src/main/java/com/suwfe/sm/teamcount3/RateActivity.java
+++ app/src/main/java/com/suwfe/sm/teamcount3/RateActivity.java
@@ -1,16 +1,23 @@
 package com.suwfe.sm.teamcount3;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class RateActivity extends AppCompatActivity {
+
+    private  final  String TAG ="Rate";
+    private  float dollarRate=0.1f;
+    private  float euroRate=0.2f;
+    private  float wonRate=0.3f;
+
     EditText rmb;
     TextView show;
     @Override
@@ -31,23 +38,54 @@ public class RateActivity extends AppCompatActivity {
             Toast.makeText(this,"plasea input",Toast.LENGTH_SHORT).show();
         }
         if(btn.getId()==R.id.dollar){
-             val = r * (1/6.7f);
-
+             show.setText(String.format("%.2f",r*dollarRate));
         }else if(btn.getId()==R.id.euro){
-            val = r *(1/11.0f);
-
+            show.setText(String.format("%.2f",r*euroRate));
         }else {
-           val = r * 500;
-
+            show.setText(String.format("%.2f",r*wonRate));
         }
-        show.setText(""+val);
-
-        }
+    }
     public void openOne(View btn){
-        Log.i("open","openOne");
-        Intent hello =new Intent(this,MainActivity.class);
-        Intent web =new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jd.com"));
-        Intent intent =new Intent(Intent.ACTION_DIAL, Uri.parse("tel:87092173"));
-        startActivity(hello);
+        openConfig();
+    }
+
+    private void openConfig() {
+        Intent config = new Intent(this, Rate2Activity.class);
+        config.putExtra("dollar_rate_key", dollarRate);
+        config.putExtra("euro_rate_key", euroRate);
+        config.putExtra("wonrate_key", wonRate);
+
+
+        //startActivity(config);
+        startActivityForResult(config, 1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.menu_set){
+            Intent config=new Intent(this,Rate2Activity.class);
+            config.putExtra("dollar_rate_key",dollarRate);
+            config.putExtra("euro_rate_key",euroRate);
+            config.putExtra("wonrate_key",wonRate);
+            startActivityForResult(config,1);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1&&resultCode==2){
+            Bundle bundle =data.getExtras();
+            dollarRate =bundle.getFloat("key_dollar",0.1f);
+            euroRate =bundle.getFloat("key_euro",0.2f);
+            wonRate =bundle.getFloat("key_won",0.3f);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
